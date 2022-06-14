67bb61f9020e8db3d0a346ea713315b5678371cb
Jonas Carvalho <jonasmcarvalho@outlook.com>
2022-06-13 21:18:02 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import {Post} from './Post'
+
+export function App() {
+
+  return (
+    <div>
+      <Post 
+        author='Jonas Carvalho'
+        content= 'loren ipsom dolor'
+      />
+
+      <Post 
+        author='Gabriel Alberto'
+        content= 'loren ipsom dolor maluo'
+      />
+
+      <Post 
+        author='Lucas Antunes'
+        content= 'loren ipsom dolor maluo'
+      />
+      
+    </div>
+  )
+}
+
